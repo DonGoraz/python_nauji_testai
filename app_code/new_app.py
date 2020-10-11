850a70c90ced103124d5f9ff44f9bdfeef42f628
DonGoraz <63724668+DonGoraz@users.noreply.github.com>
2020-10-11 11:50:34 +0300

    import files

--- app_code/new_app.py
+++ app_code/new_app.py
@@ -1,17 +1,31 @@
-class EmployeeScanner:
+import json
 
+from app_code.database import *
+# from database import DatabaseContextManager
 
-    def __init__(self, file_path):
-        self.file_path = file_path
+class EmployeeScanner():
 
+    def __init__(self, *args):
+        self.file1 = args[0]
+        self.file2 = args[1]
 
     def execute(self):
-        pass
 
+        with open(self.file1, "r") as read_file1:
+            with open(self.file2, "r") as read_file2:
+                data1 = json.load(read_file1)
+                print(type(data1["Employees"]))
+                data2 = json.load(read_file2)
+                print(data2["Feedback"])
+                print("-"*50)
 
     @property
     def employee(self):
         return self.execute()
 
 
-scanner = EmployeeScanner(company_employees.json)
+scanner = EmployeeScanner("company_employees.json", "feedback_for_employees.json")
+# scanner.employee
+
+
+create_employee_table()
